ccc96768c0a8525d5d4bb1e46e90148da20146f4
AdamDono <>
2024-07-05 13:27:10 +0200

    get all data inputed via the locast popup

--- src/app/my-form/my-form.component.ts
+++ src/app/my-form/my-form.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class MyFormComponent {
   onSubmit(form: any) {
-    const confirmationMessage = `Are you sure you want to submit the form with the following data?\n\nName: ${form.value.name}\nEmail: ${form.value.email}`;
+    const confirmationMessage = `Are you sure you want to submit the form with the following data? \n\neventname:${form.value.eventname}\n\ntickettype:${form.value.tickettype}\n\nticket:${form.value.ticket}\n\nnumber:${form.value.number}\n\nEmail:${form.value.email} `;
     if (window.confirm(confirmationMessage)) {
       // Handle form submission logic here
       console.log('Form submitted:', form.value);
