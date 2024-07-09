c2ce5546e3c3c2a5acf12d7d266aa16caf9c19ef
AdamDono <>
2024-07-09 12:02:10 +0200

    added layout on the card form

--- src/app/my-form/my-form.component.ts
+++ src/app/my-form/my-form.component.ts
@@ -3,16 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-my-form',
   templateUrl: './my-form.component.html',
-  styleUrls: ['./my-form.component.css'] // This should match the actual file path
+  styleUrls: ['./my-form.component.css'] 
 })
 export class MyFormComponent {
   onSubmit(form: any) {
+
     const confirmationMessage = `Are you sure you want to submit the form with the following data? \n\neventname:${form.value.eventname}\n\ntickettype:${form.value.tickettype}\n\nticket:${form.value.ticket}\n\nnumber:${form.value.number}\n\nEmail:${form.value.email} `;
     if (window.confirm(confirmationMessage)) {
       // Handle form submission logic here
       console.log('Form submitted:', form.value);
     } else {
       console.log('Form submission cancelled.');
+    
     }
   }
 }
